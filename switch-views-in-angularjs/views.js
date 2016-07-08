b1e87983fd28e4f7a4c4e6d5015133d327a8115c
udeyandra89 <udeya.90@gmail.com>
2016-07-08 13:13:02 +0530

    Updates

---
+++ switch-views-in-angularjs/views.js
@@ -0,0 +1,40 @@
+// create the module and name it
+	var nistantriApp = angular.module('nistantriApp', ['ngRoute']);
+
+	// routes
+	nistantriApp.config(function($routeProvider) {
+		$routeProvider
+
+			// route for the home page
+			.when('/', {
+				templateUrl : 'partials/home.html',
+				controller  : 'mainController'
+			})
+
+			// route for the about page
+			.when('/about', {
+				templateUrl : 'partials/about.html',
+				controller  : 'aboutController'
+			})
+
+			// route for the service page
+			.when('/service', {
+				templateUrl : 'partials/services.html',
+				controller  : 'serviceController'
+			});
+	
+	});
+
+	// create the controller and inject Angular's $scope
+	nistantriApp.controller('mainController', function($scope) {
+		// create a message to display in our view
+		$scope.info = 'Welcome to Nistantri';
+	});
+
+	nistantriApp.controller('aboutController', function($scope) {
+		$scope.info = 'About Nistantri';
+	});
+
+	nistantriApp.controller('serviceController', function($scope) {
+		$scope.info = 'Building E-learning Authoring tool';
+	});

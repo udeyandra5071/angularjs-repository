02a2ade8182069535b9bb55665f33b8ff73d3f46
udeyandra89 <udeya.90@gmail.com>
2016-07-25 18:40:57 +0530

    Updated

--- final-layout-with-angularjs/js/nistantriApp.js
+++ final-layout-with-angularjs/js/nistantriApp.js
@@ -12,10 +12,35 @@ function addactive(){
     });
     
 }
-nistantriApp.controller("homecontroller",function($scope,$http){
+
+ function swipeme(){
+        var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton:".swiper-button-next",
+           prevButton:".swiper-button-prev",
+           simulateTouch:true,
+           autoplay:6000,
+           onInit:function(){$(".caption").show(); } 
+            
+    }); 
+     swiper.on('slideChangeStart', function () {
+    $(".caption").hide(); 
+});   
+swiper.on('slideChangeEnd', function () {
+    $(".caption").show(); 
+});
+
+    }
+nistantriApp.controller("homecontroller",function($scope,$http,$timeout){
     $scope.info="Udeyandra";
       $http({method: 'POST', url: 'json/header.json'}).success(function(data) {
 $scope.header = data;
     console.log(data);
+});   
+$http({method: 'POST', url: 'json/banner.json'}).success(function(data) {
+$scope.bannerdata = data;
+    console.log(data);
 });
+$timeout(swipeme, 6);
 });
